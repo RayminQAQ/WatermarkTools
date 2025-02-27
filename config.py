212eb1984a32e16230bbd000a290eaa427455670
RayminQAQ <raymin0425@gmail.com>
2025-02-28 01:24:47 +0800

    [Update]: adding more argument items

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 """
 arg list:
-- arg.watermark: the name of the watermark algorithm
 - args.dataset: the name of dataset
 - args.dataset_path: the path of args.dataset, it will be automatically download if None
+- args.watermark: the name of the watermark algorithm
+- args.model: the name of the watermark algorithm
 
 """
 
